78f3d6ddc510c94b56fa35528c77bd839aade736
jina-leemon <103284121+jina-leemon@users.noreply.github.com>
2022-05-04 06:51:43 -0700

    Code for skill demo

---
+++ SkillDemoTest_1.java
@@ -0,0 +1,15 @@
+// Includes jUnit code that tests function
+import static org.junit.Assert.*;
+import org.junit.*;
+
+// To run this command: 
+//  javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar SkillDemoTest_1.java
+//  java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore SkillDemoTest_1.java
+
+public class SkillDemoTest_1{
+    @Test
+    public void average(){
+        int[] array = {10, 12, 14, 12};
+        assertEquals(10, SkillDemo_1.average(array)); // inital test that fails
+    }
+}
--- SkillDemo_1.java
+++ SkillDemo_1.java
@@ -1,1 +1,12 @@
+//code that has a function
 
+public class SkillDemo_1{
+    public static int average(int[] n){
+        int sum = 0;
+        for(int i : n) {
+            sum += i;
+        }
+        
+        return sum/ n.length;
+    }
+}
